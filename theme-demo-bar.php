9a2dadc86d0d7247ba50c8d7b4822fe934c7fa33
Dean Sas <dean.sas@automattic.com>
2023-12-21 09:34:24 +0000

    Bring demo bar up to date with simple demo bar (#7)
    Several changes:
     * We don't sell themes as one-off purchases now so the message no longer displays the price
     * The CTA now goes to the theme showcase - the existing one used here is broken
     * Some small other code clean-up changes - removing unused variables.

--- theme-demo-bar.php
+++ theme-demo-bar.php
@@ -188,16 +188,6 @@ class Theme_Demo_Sites_Display {
 	}
 
 	/**
-	 * Get the premium theme slug
-	 *
-	 * @param string $stylesheet stylesheet slug
-	 * @return string
-	 */
-	private function get_theme_slug( $stylesheet ) {
-		return "premium/{$stylesheet}";
-	}
-
-	/**
 	 * Check if the theme is premium for a given stylesheet
 	 *
 	 * @param string $stylesheet stylesheet slug
@@ -238,32 +228,19 @@ class Theme_Demo_Sites_Display {
 		// 'theme' and 'demo-blog'
 		$url = add_query_arg(
 			array(
-				'theme'   => rawurlencode( $this->get_theme_slug( $theme->stylesheet ) ),
 				'premium' => $this->is_premium_theme( $theme->stylesheet ) ? 'true' : false,
 				'ref'     => 'demo-blog',
 			),
-			'https://wordpress.com/start/with-theme'
+			'https://wordpress.com/theme/' . rawurlencode( $theme->stylesheet )
 		);
 
 		$title    = __( 'Start your WordPress.com site with this theme.' );
 		$tab_text = __( 'Sign Up Now' );
-		$text     = __( 'Start a site with this theme.' );
-		$button   = __( 'Activate' );
 
 		if ( $this->is_premium_theme( $theme->stylesheet ) ) {
-			$tab_text = __( 'Purchase' );
-			$text     = __( 'Create a site and purchase this theme to start using it now.' );
-			$button   = __( 'Purchase &amp; Activate' );
+			$tab_text = __( 'Start now' );
 		}
 
-		// if the site is premium, set $theme_price
-		$theme_price = '';
-
-		$premium_theme_data = $this->get_premium_theme_data( $theme->stylesheet );
-
-		if ( $premium_theme_data && $this->is_premium_theme( $theme->stylesheet ) && $theme->is_allowed( 'network' ) ) {
-			$theme_price = '<span class="theme-price">' . $premium_theme_data->cost . '</span>';
-		}
 		?>
 
 		<div id="demosite-activate-wrap" class="demosite-activate">
@@ -276,7 +253,6 @@ class Theme_Demo_Sites_Display {
 				<a class="demosite-activate-trigger" href="<?php echo esc_url( $url ); ?>">
 					<?php
 						echo $tab_text; //phpcs:ignore
-						echo $theme_price; //phpcs:ignore
 					?>
 				</a>
 				<a class="demosite-activate-cta-arrow" href="<?php echo esc_url( $url ); ?>">
